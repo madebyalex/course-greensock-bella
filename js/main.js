2d6ac5422e065ed981621c14e56cbfa82edd52e2
Alexander <hello@alexander.works>
2021-03-11 22:59:56 +0200

    Add mouse hover effect for nav links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
 
-function init(){
-    
-    // start here
+function initNavigation() {
+  const mainNavLinks = gsap.utils.toArray('.main-nav a');
 
+  mainNavLinks.forEach((link) => {
+    link.addEventListener('mouseleave', (e) => {
+      // Add class
+      link.classList.add('animate-out');
+
+      setTimeout(() => {
+        // Remove class
+        link.classList.remove('animate-out');
+      }, 300);
+    });
+  });
 }
 
-window.addEventListener('load', function(){
-    init();
+function init() {
+  initNavigation();
+}
+
+window.addEventListener('load', function () {
+  init();
 });
