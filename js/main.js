1babb5ac89ef0e9367618521c0a018a88762945a
Alexander <hello@alexander.works>
2021-03-12 03:47:26 +0200

    Add parallax animation for the Hero section

--- js/main.js
+++ js/main.js
@@ -42,12 +42,60 @@ function initNavigation() {
     },
     onEnter: ({ direction }) => navAnimation(direction),
     onLeaveBack: ({ direction }) => navAnimation(direction),
-    markers: true,
+    // markers: true,
+  });
+}
+
+function initHeaderTilt() {
+  document.querySelector('header').addEventListener('mousemove', moveImages);
+}
+
+function moveImages(e) {
+  const { offsetX, offsetY, target } = e;
+  const { clientWidth, clientHeight } = target;
+  // console.log(offsetX, offsetY, clientWidth, clientHeight);
+
+  const xPos = offsetX / clientWidth - 0.5;
+  const yPos = offsetY / clientHeight - 0.5;
+
+  const leftImages = gsap.utils.toArray('.hg__left .hg__image');
+  const rightImages = gsap.utils.toArray('.hg__right .hg__image');
+
+  const modifier = (index) => index * 1.2 + 0.5;
+
+  // Move left images
+  leftImages.forEach((image, index) => {
+    gsap.to(image, {
+      duration: 1.2,
+      x: xPos * 20 * modifier(index),
+      y: yPos * 30 * modifier(index),
+      rotationY: xPos * 40,
+      rotationX: yPos * 10,
+    });
+  });
+
+  // Move right images
+  rightImages.forEach((image, index) => {
+    gsap.to(image, {
+      duration: 1.2,
+      x: xPos * 20 * modifier(index),
+      y: -yPos * 30 * modifier(index),
+      rotationY: xPos * 40,
+      rotationX: yPos * 10,
+    });
+  });
+
+  gsap.to('.decor__circle', {
+    duration: 1.7,
+    x: 80 * xPos,
+    y: 80 * yPos,
+    ease: 'Power4.out',
   });
 }
 
 function init() {
   initNavigation();
+  initHeaderTilt();
 }
 
 window.addEventListener('load', function () {
