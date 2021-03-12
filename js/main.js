3f120dc275953a7c2cba30e4e67931a12ae5703d
Alexander <hello@alexander.works>
2021-03-12 02:53:10 +0200

    Animate the main nav in / out by scrolling

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 function initNavigation() {
   const mainNavLinks = gsap.utils.toArray('.main-nav a');
+  const mainNavLinksReversed = gsap.utils.toArray('.main-nav a').reverse();
 
   mainNavLinks.forEach((link) => {
     link.addEventListener('mouseleave', (e) => {
@@ -13,6 +14,35 @@ function initNavigation() {
         link.classList.remove('animate-out');
       }, 300);
     });
+  });
+
+  function navAnimation(direction) {
+    const scrollingDown = direction === 1;
+    const links = scrollingDown ? mainNavLinks : mainNavLinksReversed;
+    const navContainer = document.querySelector('.main-nav ul');
+
+    gsap.set(navContainer, { perspective: 500 });
+
+    return gsap.to(links, {
+      duration: 0.4,
+      stagger: 0.08,
+      ease: 'Power4.out',
+      autoAlpha: () => (scrollingDown ? 0 : 1),
+      y: () => (scrollingDown ? 20 : 0),
+      rotationX: () => (scrollingDown ? 45 : 0),
+    });
+  }
+
+  ScrollTrigger.create({
+    start: 100,
+    end: 'bottom bottom-=20',
+    toggleClass: {
+      targets: 'body',
+      className: 'has-scrolled',
+    },
+    onEnter: ({ direction }) => navAnimation(direction),
+    onLeaveBack: ({ direction }) => navAnimation(direction),
+    markers: true,
   });
 }
 
