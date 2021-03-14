4f79069b2af6373e352627fefcf834c552fd3ffc
Alexander <hello@alexander.works>
2021-03-14 18:58:08 +0200

    Parallax images: Implement fixed steps navigation and highlight the current step

--- 04-parallax-images/js/main.js
+++ 04-parallax-images/js/main.js
@@ -19,8 +19,44 @@ function initParallaxImages() {
   });
 }
 
+function initPinSteps() {
+  ScrollTrigger.create({
+    trigger: '.fixed-nav',
+    start: 'top center',
+    endTrigger: '#stage4',
+    end: 'center center',
+    pin: true,
+    // markers: true,
+  });
+
+  const getViewportHeight = () => {
+    const vh = Math.max(
+      document.documentElement.clientHeight || 0,
+      window.innerHeight || 0
+    );
+    return vh;
+  };
+
+  gsap.utils.toArray('.stage').forEach((stage, index) => {
+    const navLinks = gsap.utils.toArray('.fixed-nav li');
+
+    ScrollTrigger.create({
+      trigger: stage,
+      start: 'top center',
+      //   end: 'bottom center',
+      end: () => `+=${stage.clientHeight + getViewportHeight() / 10}`,
+      toggleClass: {
+        targets: navLinks[index],
+        className: 'is-active',
+      },
+      markers: true,
+    });
+  });
+}
+
 function init() {
   initParallaxImages();
+  initPinSteps();
 }
 
 window.addEventListener('load', function () {
