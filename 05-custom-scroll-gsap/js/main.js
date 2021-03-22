8507c61da97a44342a227dc7df490512ea44f44c
Alexander <hello@alexander.works>
2021-03-23 01:55:39 +0200

    Smooth scrolling with GSAP: Part 1

---
+++ 05-custom-scroll-gsap/js/main.js
@@ -0,0 +1,100 @@
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+function initParallaxImages() {
+  const allSections = gsap.utils.toArray('.with-parallax');
+
+  allSections.forEach((section) => {
+    const image = section.querySelector('img');
+
+    gsap.to(image, {
+      yPercent: 20,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: section,
+        start: 'top bottom',
+        scrub: true,
+        // markers: true,
+      },
+    });
+  });
+}
+
+function initPinSteps() {
+  ScrollTrigger.create({
+    trigger: '.fixed-nav',
+    start: 'top center',
+    endTrigger: '#stage4',
+    end: 'center center',
+    pin: true,
+    // markers: true,
+  });
+
+  const getViewportHeight = () => {
+    const vh = Math.max(
+      document.documentElement.clientHeight || 0,
+      window.innerHeight || 0
+    );
+    return vh;
+  };
+
+  const updateBodyColor = (color) => {
+    // gsap.to('.fill-background', { backgroundColor: color, ease: 'none' });
+    document.documentElement.style.setProperty('--bcg-fill-color', color);
+  };
+
+  gsap.utils.toArray('.stage').forEach((stage, index) => {
+    const navLinks = gsap.utils.toArray('.fixed-nav li');
+
+    ScrollTrigger.create({
+      trigger: stage,
+      start: 'top center',
+      //   end: 'bottom center',
+      end: () => `+=${stage.clientHeight + getViewportHeight() / 10}`,
+      toggleClass: {
+        targets: navLinks[index],
+        className: 'is-active',
+      },
+      //   markers: true,
+      onEnter: () => updateBodyColor(stage.dataset.color),
+      onEnterBack: () => updateBodyColor(stage.dataset.color),
+    });
+  });
+}
+
+function initScrollTo() {
+  gsap.utils.toArray('.fixed-nav a').forEach((link) => {
+    const target = link.getAttribute('href');
+
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      gsap.to(window, {
+        duration: 0.9,
+        scrollTo: {
+          y: target,
+          offsetY: 80,
+          ease: 'Power2.out',
+        },
+      });
+    });
+  });
+}
+
+function init() {
+  // initParallaxImages();
+  // initPinSteps();
+  // initScrollTo();
+}
+
+window.addEventListener('load', function () {
+  init();
+});
+
+let container = document.querySelector('#scroll-container');
+let height;
+
+function setHeight() {
+  height = container.clientHeight;
+  document.body.style.height = `${height}px`;
+}
+
+ScrollTrigger.addEventListener('refreshInit', setHeight);
